f02a3adf52a00a1661203203e111c7fb2b4c87f2
Reshma Nair <reshmanair4567@gmail.com>
2020-07-22 22:04:28 -0400

    07/22 commit

--- 30days/Leetcode_Longest_common_prefix.py
+++ 30days/Leetcode_Longest_common_prefix.py
@@ -1,15 +1,12 @@
 def prefix(strs):
     strs.sort(key=lambda strs:len(strs))
     temp=strs[0]
-    flag=True
     for i in range(len(strs)):
         for j in range(len(strs[0])):
             if temp[j]!=strs[i][j]:
                 temp= temp[:j]
-                flag=False
                 break
     return temp
-
 
 if __name__=="__main__":
     strs=["flow","flowerpot","flower"]
