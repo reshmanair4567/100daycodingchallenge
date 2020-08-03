96f2f8b4dfac6628b22744a147e4b2abab50e11e
Reshma Nair <reshmanair4567@gmail.com>
2020-08-02 21:48:59 -0400

    08/02 Commit

--- Python/August_Detect_Capital.py
+++ Python/August_Detect_Capital.py
@@ -4,7 +4,57 @@ class Solution:
         match2=True
         match3=True
         n=len(word)
+
+        '''Method 1'''
+        #all caps
+        if word[0].isupper() and word[1].isupper():
+            for i in range(2,n):
+                if word[i].isupper():
+                    match1=True
+                else:
+                    match1=False
+            if match1:
+                return True
+            else:
+                return False
+        else:
+            #one start caps or no caps 
+            if word[0].isupper():
+                for i in range(1,n):
+                    if word[i].islower():
+                        match2=True
+                    else:
+                        match2=False
+                if match2:
+                    return True
+                else:
+                    return False
+
+            else:
+                for i in range(n):
+                    if word[i].isupper():
+                        match3=False
+                    else:
+                        match3=True
+                if match3:
+                    return True
+                else:
+                    return False
         
+        
+            
+            
+
+                
+
+
+
+            
+
+
+
+        '''
+        Method 2 
         #all caps
         for i in range(n):
             if not word[i].isupper():
@@ -20,16 +70,19 @@ class Solution:
         if match2:
             return True
         #first cap
-        for i in range(1,n):
-            if not (word[0].isupper() and word[i].islower()):
-                match3=False
-                
+        if not word[0].isupper():
+            match3=False
+        if match3:
+            for i in range(1,n):
+                if word[i].isupper():
+                    match3=False
+
         if match3:
             return True
         else:
             return False
         
-            
+    '''  
         
 if __name__=="__main__":
     inp=input()
