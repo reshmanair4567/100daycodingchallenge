1f05b613e373d21d08252885d35e1bb60e8c0fac
Reshma Nair <reshmanair4567@gmail.com>
2020-08-02 21:55:32 -0400

    08/02 Commit

--- Python/August_Detect_Capital.py
+++ Python/August_Detect_Capital.py
@@ -1,60 +1,35 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        match1=True
-        match2=True
-        match3=True
+
         n=len(word)
 
         '''Method 1'''
         #all caps
         if word[0].isupper() and word[1].isupper():
             for i in range(2,n):
-                if word[i].isupper():
-                    match1=True
-                else:
-                    match1=False
-            if match1:
-                return True
-            else:
-                return False
+                if not word[i].isupper():
+                    return False
         else:
             #one start caps or no caps 
             if word[0].isupper():
                 for i in range(1,n):
-                    if word[i].islower():
-                        match2=True
-                    else:
-                        match2=False
-                if match2:
-                    return True
-                else:
-                    return False
+                    if not word[i].islower():
+                        return False
 
             else:
                 for i in range(n):
                     if word[i].isupper():
-                        match3=False
-                    else:
-                        match3=True
-                if match3:
-                    return True
-                else:
-                    return False
+                        return False
+                    
+        return True
         
-        
-            
-            
-
-                
-
-
-
-            
-
-
 
         '''
         Method 2 
+        match1=True
+        match2=True
+        match3=True
+        n=len(word)
         #all caps
         for i in range(n):
             if not word[i].isupper():
