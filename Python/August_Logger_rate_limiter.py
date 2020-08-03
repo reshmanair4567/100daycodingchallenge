ecb02035988bc71e7a6fab62b1a38292b08b5d5d
Reshma Nair <reshmanair4567@gmail.com>
2020-08-02 22:32:26 -0400

    08/02 - Commit

--- Python/August_Logger_rate_limiter.py
+++ Python/August_Logger_rate_limiter.py
@@ -1,18 +1,8 @@
 class Logger:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.data=dict()
         
-
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
+    def shouldPrintMessage(self,timestamp, message):
         if message not in self.data or timestamp-self.data[message]>=10:
             self.data[message]=timestamp
             return True
@@ -21,12 +11,7 @@ class Logger:
 
 
 if __name__=="__main__":
-    inp=[[],[1,"foo"],[2,"bar"],[3,"foo"],[8,"bar"],[10,"foo"],[11,"foo"]]
-    obj=Logger()
-    param_1 = obj.shouldPrintMessage(timestamp,message)
-
-
-
-    # print("p",len(inp))
-    # for i in range(len(inp)):
-        # c.shouldPrintMessage(inp[i][0],inp[i][1])
+    inp=[[1,"foo"],[2,"bar"],[3,"foo"],[8,"bar"],[10,"foo"],[11,"foo"]]
+    c=Logger()
+    for i in range(len(inp)):
+        print(c.shouldPrintMessage(inp[i][0],inp[i][1]))
--- Python/Stratchpad.py
+++ Python/Stratchpad.py
@@ -127,3 +127,7 @@ class Solution:
         return len(chars)
 
 
+
+
+'''Returns true if the message should be printed in the given timestamp, otherwise returns false.If this method returns false, the message will not be printed.The timestamp is in seconds granularity.'''
+        
