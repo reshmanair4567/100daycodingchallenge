661c0ab9614aefcf3d11b44174369f6db4505043
Reshma Nair <reshmanair4567@gmail.com>
2020-08-04 22:49:55 -0400

    08/04 - Commit

--- Python/Codebasics_BinarySearchTree.py
+++ Python/Codebasics_BinarySearchTree.py
@@ -53,8 +53,6 @@ class BinarySearchTreeNode:
                 return False
 
 
-
-
 def build_tree(elements):
     root=BinarySearchTreeNode(elements[0])
 
@@ -63,7 +61,10 @@ def build_tree(elements):
     return root
 
 if __name__=="__main__":
-    nums=[17,4,1,20,9,23,18,34]
+    nums=[17,4,-1,20,9,23,18,-1,4,20,20,20,20,34]
     numbers_tree=build_tree(nums)
-    print(numbers_tree.in_order_traversal())
-    print(" Is 4 in the list ? : ",numbers_tree.search(4))
+    print("Inorder Traveresed BST is:  ",numbers_tree.in_order_traversal())
+    print("Miniumum number is BST :    ",min(numbers_tree.in_order_traversal()))
+    print("Maximum number in BST:      ",max(numbers_tree.in_order_traversal()))
+    print("Sum of all elements in BST: ",sum(numbers_tree.in_order_traversal()))
+    print(" Is 4 in the list ? :       ",numbers_tree.search(4))
