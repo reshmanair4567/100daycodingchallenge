b7c8115d42cb9423259e58bb02f49ff0b11cbb35
Reshma Nair <reshmanair4567@gmail.com>
2020-08-05 21:51:06 -0400

    08/05 - Commit

--- Python/August_Add_Search_Work.py
+++ Python/August_Add_Search_Work.py
@@ -1,39 +1,96 @@
-class WordDictionary:
-
+class TrieNode():
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.word=[]
-        
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+class WordDictionary(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+
+    def search(self, word):
+        node = self.root
+        self.res = False
+        self.dfs(node, word)
+        return self.res
+    
+    def dfs(self, node, word):
+        if not word:
+            if node.isWord:
+                self.res = True
+            return 
+        if word[0] == ".":
+            for n in node.children.values():
+                self.dfs(n, word[1:])
+        else:
+            node = node.children.get(word[0])
+            if not node:
+                return 
+            self.dfs(node, word[1:])
+
+'''Method -2 
+class WordDictionary:
+    def __init__(self):
+        self.trie = {}
 
     def addWord(self, word: str) -> None:
-        """
-        Adds a word into the data structure.
-        """
-        elements=[]
-        if word:
-            elements.append(word)
-        print(elements)
-        return elements
+        node = self.trie
+        for c in word:
+            node = node.setdefault(c, {})
+        node['$'] = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
-        """
-        if word==elements[0]:
-            return True
-        for i in range(len(elements[0])):
-            if word[i:]==elements[0][i:]:
-                return True
+        return self.searchNode(self.trie, word)
+
+    def searchNode(self, node, word: str) -> bool:
+        for i, c in enumerate(word):
+            if c == '.':
+                return any(self.searchNode(node[w], word[i+1:]) for w in node if w != '$')
+            if c not in node: return False
+            node = node[c]
+        return '$' in node
+    '''
+
+    ''' Method-3
+    class WordNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = WordNode()
+        
+    def addWord(self, word):
+        node = self.root
+        for w in word:
+            if w in node.children:
+                node = node.children[w]
+            else:
+                node.children[w] = WordNode()
+                node = node.children[w]
+        node.isEnd = True
+                
+    def search(self, word):
+        stack = [(self.root,word)]
+        while stack:
+            node, w = stack.pop()
+            if not w:
+                if node.isEnd:
+                    return True
+            elif w[0]=='.':
+                for n in node.children.values():
+                    stack.append((n,w[1:]))
+            else:
+                if w[0] in node.children:
+                    n = node.children[w[0]]
+                    stack.append((n,w[1:]))
         return False
-def my(elements):
-
-
+    '''
 if __name__=="__main__":
-    word=["bad","dad"]
-    for i in range(0,len(word)):
-        obj = WordDictionary().addWord(word[i])
-    print("obj:",obj)
-    # param_2 = obj.search(word)
-    # print(param_2)
+
+    c=WordDictionary().search('add')
