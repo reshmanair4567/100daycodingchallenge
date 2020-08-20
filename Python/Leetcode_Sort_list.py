3e2f0ba1d7223fdfb6da12e84f2931e82a2aecc5
Reshma Nair <reshmanair4567@gmail.com>
2020-08-19 22:57:10 -0400

    08/19 - Commit

--- Python/Leetcode_Sort_list.py
+++ Python/Leetcode_Sort_list.py
@@ -1,10 +1,10 @@
 #Sort a Linked List - Solution
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution(object):
     def merge(self, h1, h2):
@@ -12,6 +12,7 @@ class Solution(object):
         while h1 and h2:
             if h1.val < h2.val:
                 tail.next, tail, h1 = h1, h1, h1.next
+                
             else:
                 tail.next, tail, h2 = h2, h2, h2.next
     
@@ -29,6 +30,45 @@ class Solution(object):
 
         return self.merge(*map(self.sortList, (head, slow)))
 
-if __name__=="__main__":
 
-    print(Solution().merge())
+#------------------------------------ OR --------------------
+
+
+# merge sort, recursively 
+def sortList(self, head):
+    if not head or not head.next:
+        return head
+    # divide list into two parts
+    fast, slow = head.next, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    second = slow.next
+    # cut down the first part
+    slow.next = None
+    l = self.sortList(head)
+    r = self.sortList(second)
+    return self.merge(l, r)
+    
+# merge in-place without dummy node        
+def merge(self, l, r):
+    if not l or not r:
+        return l or r
+    if l.val > r.val:
+        l, r = r, l
+    # get the return node "head"
+    head = pre = l
+    l = l.next
+    while l and r:
+        if l.val < r.val:
+            l = l.next
+        else:
+            nxt = pre.next
+            pre.next = r
+            tmp = r.next
+            r.next = nxt
+            r = tmp
+        pre = pre.next
+    # l and r at least one is None
+    pre.next = l or r
+    return head
