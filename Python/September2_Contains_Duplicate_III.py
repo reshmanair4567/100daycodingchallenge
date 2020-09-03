cca7d7fa3f9cedcab7683008a10a12e3c1b4659d
Reshma Nair <reshmanair4567@gmail.com>
2020-09-03 00:36:58 -0400

    09/02 - Commit

--- Python/September2_Contains_Duplicate_III.py
+++ Python/September2_Contains_Duplicate_III.py
@@ -9,3 +9,20 @@ class Solution:
                 if abs(nums[i] - nums[j]) <= t:
                     return True
         return False
+
+'''OR'''
+class Solution:
+
+def containsNearbyAlmostDuplicate(self, nums, k, t):
+    if k < 1 or t < 0:
+        return False
+    dic = collections.OrderedDict()
+    for n in nums:
+        key = n if not t else n // t
+        for m in (dic.get(key - 1), dic.get(key), dic.get(key + 1)):
+            if m is not None and abs(n - m) <= t:
+                return True
+        if len(dic) == k:
+            dic.popitem(False)
+        dic[key] = n
+    return False
