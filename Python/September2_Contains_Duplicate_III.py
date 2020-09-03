f2a81fe0ea1697b6779a62360dfabfbe34ee6b65
Reshma Nair <reshmanair4567@gmail.com>
2020-09-03 00:08:43 -0400

    09/02-Commit

---
+++ Python/September2_Contains_Duplicate_III.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        # regular duplication
+        if t == 0 and len(set(nums)) == len(nums):
+            return False
+        
+        for i in range(len(nums)):
+            for j in range(i+1, min(i+k+1, len(nums))):
+                if abs(nums[i] - nums[j]) <= t:
+                    return True
+        return False
